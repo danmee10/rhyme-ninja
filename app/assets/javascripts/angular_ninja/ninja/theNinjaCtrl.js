681aaadafed248cb64b8634e8a638bac75839eb5
Dan Mee <dmee@Dans-iMac.local>
2015-04-16 13:20:14 -0600

    protect poems from being edited by users other than the creator.  need js flash

--- app/assets/javascripts/angular_ninja/ninja/theNinjaCtrl.js
+++ app/assets/javascripts/angular_ninja/ninja/theNinjaCtrl.js
@@ -1,11 +1,25 @@
-app.controller('theNinjaCtrl', ['$scope', 'Rhyme', '$location', 'rhyme', '$stateParams', function ($scope, Rhyme, $location, rhyme, $stateParams){
+app.controller('theNinjaCtrl', ['$scope', 'Rhyme', '$location', 'rhyme', '$stateParams', 'User', '$http', function ($scope, Rhyme, $location, rhyme, $stateParams, User, $http){
   'use strict'
 
 
+  var rhymeById = function(id) {
+    return _.find($scope.rhymes, function(r) {
+      return r.id == id;
+    });
+  };
+
   if (rhyme.original_text === '') {
-    Rhyme.get({user_id: id, id: $stateParams.rhyme_id, authenticity_token: token}, function(r) {
-        $.extend(rhyme, r);
-        $scope.rhyme = rhyme;
+    User.rhymes({user_id: id}, function(data) {
+      $scope.rhymes = data.rhymes;
+      $scope.rhyme = rhymeById($stateParams.rhyme_id);
+      if (typeof $scope.rhyme !== 'undefined') {
+        $.extend(rhyme, $scope.rhyme);
+      } else {
+        // angular flash
+        window.location.hash = '/';
+        console.log("http1 --> ", "Angualr flash")
+        alert("broken")
+      }
     });
   } else {
     $scope.rhyme = rhyme;
