b14e609d42fc39744321ac261c57ed21bd034ff5
Dan Mee <dmee@Dans-iMac.local>
2015-04-16 19:35:51 -0600

    renamed angular_flash to angularFlash

---
+++ app/assets/javascripts/angular_ninja/angularFlash.js
@@ -0,0 +1,12 @@
+app.factory('angularFlash', [function(){
+  var flash = {
+    alertDanger: function (msg) {
+      if ($('#js_flash').css('display') === 'none') {
+        $('#js_flash').show();
+        $('#js_flash').addClass('alert-danger');
+        $('#js_flash').append(msg);
+      }
+    }
+  };
+  return flash;
+}])
