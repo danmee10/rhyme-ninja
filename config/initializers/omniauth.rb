aa106d180077c863178acf36e8673ed8f3621fb4
Dan Mee <dmee@Dans-iMac.local>
2015-04-17 17:32:00 -0600

    force login on google and facebook.  instagram doesn't allow this for somereason

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,9 +5,12 @@ Rails.application.config.middleware.use OmniAuth::Builder do
       force_login: 'true'
     }
   }
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
+  {
+    prompt: 'consent'
+  }
   provider :instagram, ENV['INSTAGRAM_ID'], ENV['INSTAGRAM_SECRET']
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], auth_type: 'reauthenticate'
   provider :identity, on_failed_registration: lambda { |env|
     SessionsController.action(:new).call(env)
   }
