03170fd052f1ae6ecccc30f814501bca6017f8fd
Dan Mee <dmee@Dans-iMac.local>
2015-04-16 18:30:56 -0600

    clicking rhyme link on my-rhymes page now sets rhyme service rhyme

--- app/assets/javascripts/angular_ninja/user/userRhymesCtrl.js
+++ app/assets/javascripts/angular_ninja/user/userRhymesCtrl.js
@@ -1,10 +1,17 @@
-app.controller('userRhymesCtrl', ['$scope', 'User', function ($scope, User){
+app.controller('userRhymesCtrl', ['$scope', 'User', 'rhyme', function ($scope, User, rhyme){
   'use strict'
 
 
   User.rhymes({user_id: id}, function(data) {
     $scope.rhymes = data.rhymes;
   });
+
+  $scope.setRhyme = function(id) {
+    var selection = _.find($scope.rhymes, function(r) {
+      return r.id === id;
+    });
+    $.extend(rhyme, selection)
+  };
 
 
 }]);
