3061d013e9865a7825d5f52026cfc65c2b48d87e
Dan Mee <dmee@Dans-iMac.local>
2015-04-17 16:52:40 -0600

    set twitter to force login, and set omniauth logger to rails logger, and added route to account for omniauth failures (need to test in env other than dev)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['twitter_key'], ENV['twitter_secret']
+  provider :twitter, ENV['twitter_key'], ENV['twitter_secret'],
+  {
+    authorize_params: {
+      force_login: 'true'
+    }
+  }
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
   provider :instagram, ENV['INSTAGRAM_ID'], ENV['INSTAGRAM_SECRET']
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
@@ -7,3 +12,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     SessionsController.action(:new).call(env)
   }
 end
+
+OmniAuth.config.logger = Rails.logger
