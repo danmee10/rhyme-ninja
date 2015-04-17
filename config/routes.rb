dae3db34642718876ef9f22e82c3f67e29c284a3
Dan Mee <dmee@Dans-iMac.local>
2015-04-17 16:52:40 -0600

    set twitter to force login, and set omniauth logger to rails logger

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "angular#front_end"
 
   match '/auth/:provider/callback', :to => 'sessions#create', via: [:get, :post]
+  match '/auth/failure', to: redirect('/'), via: :get
 
   get 'sign_out', to: "sessions#destroy"
   get 'create_account', to: "sessions#new"
