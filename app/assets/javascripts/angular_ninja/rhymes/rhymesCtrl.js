ccbd8b12f56d0fce37ed459e7ebcb1ac143005ca
Dan Mee <dmee@Dans-iMac.local>
2015-05-22 14:56:53 -0600

    moved Rhyme related js files into their own directory

---
+++ app/assets/javascripts/angular_ninja/rhymes/rhymesCtrl.js
@@ -0,0 +1,23 @@
+app.controller('rhymesCtrl', ['$scope', '$stateParams', '$http', function ($scope, $stateParams, $http){
+  'use strict';
+
+  // right now I don't see any reason to create a new angular
+  // resource for public rhymes...
+  $http.get('/public_rhymes.json').success(function(data){
+    $scope.allRhymes = data;
+    filterRhymes();
+  });
+
+  console.log("$stateParams --> ", $stateParams)
+  var filterRhymes = function() {
+    if (typeof $stateParams.titleQuery !== 'undefined' || $stateParams.titleQuery === '') {
+      $scope.filteredRhymes = _.filter($scope.allRhymes, function(r){
+        var re = new RegExp($stateParams.titleQuery);
+        return re.test(r.title)
+      });
+    } else {
+      $scope.filteredRhymes = $scope.allRhymes;
+    }
+  }
+
+}]);
