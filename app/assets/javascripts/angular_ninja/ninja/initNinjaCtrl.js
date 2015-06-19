4f53f36d2d87cf97d533749a9c953e4946942fea
Dan Mee <danmee10@gmail.com>
2015-06-18 18:28:23 -0600

    added syllable_pattern and visibility to fields that save when authenticated user creates a new rhyme

--- app/assets/javascripts/angular_ninja/ninja/initNinjaCtrl.js
+++ app/assets/javascripts/angular_ninja/ninja/initNinjaCtrl.js
@@ -4,7 +4,8 @@ app.controller('initNinjaCtrl', ['$scope', 'Rhyme', '$location', '$cookies', fun
   $scope.rhyme = {
     title: '',
     original_text: '',
-    visibility: 'public_rhyme'
+    visibility: 'public_rhyme',
+    syllable_pattern: '10'
   }
 
   $scope.initNinja = function() {
@@ -25,6 +26,8 @@ app.controller('initNinjaCtrl', ['$scope', 'Rhyme', '$location', '$cookies', fun
     Rhyme.save({user_id: userId,
                   title: $scope.rhyme.title,
           original_text: $scope.rhyme.original_text,
+       syllable_pattern: $scope.rhyme.syllable_pattern,
+             visibility: $scope.rhyme.visibility,
      authenticity_token: token}, function(r){
         $location.path('/ninja/' + r.id);
     });
